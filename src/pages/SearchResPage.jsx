73e94d6fc93da8996c6ccef371b3119b2ebd4e4f
mjn81 <mjjnn81@gmail.com>
2022-02-13 01:03:42 +0330

    Structure Implementation Complete - redux and API implementation left

--- src/pages/SearchResPage.jsx
+++ src/pages/SearchResPage.jsx
@@ -4,8 +4,8 @@ const SearchResPage = () => {
     const {key} = useParams();
     // TODO: because of api shortcomings this part will be added later
     return (
-        <div>
-            Your results will be here
+        <div className="text-4xl flex items-center justify-center text-center h-screen text-gray-600">
+            <p>Your results will be here soon...</p>
         </div>
     );
 };
