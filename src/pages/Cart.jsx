7c737e0a99f88ecae14bfe33b7d9db519adb2cfe
mjn81 <mjjnn81@gmail.com>
2022-02-13 00:08:57 +0330

    items Implemented

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,5 @@
-import {SimpleCard} from "../components/core";
+import {Button, SimpleCard} from "../components/core";
+import {Link} from "react-router-dom";
 
 const Cart = () => {
     return (
@@ -9,10 +10,24 @@ const Cart = () => {
                         <h1 className="text-3xl">Cart</h1>
                     </div>
                     <div className="mt-4">
+                        {/* TODO:static card list until redux implementation */}
                         <SimpleCard>
-                            <div className="grid grid-cols-4">
-                                <div className="col-span-1">
+                            <div className="grid grid-cols-5 gap-6">
+                                <Link to="">
                                     <img src="https://via.placeholder.com/1000" alt="lsd"/>
+                                </Link>
+                                <div className="col-span-4 flex flex-col justify-between">
+                                    <Link to="dsjkn" className="w-fit text-2xl
+                                    hover:text-secondary-200">title</Link>
+                                    <p>
+                                        Amount : 5
+                                    </p>
+                                    <div className="bg-rose-500 rounded-lg">
+                                        <Button isFull={true} noBorder={true} textColor="white">
+                                            Remove Item
+                                        </Button>
+                                    </div>
+
                                 </div>
                             </div>
                         </SimpleCard>
